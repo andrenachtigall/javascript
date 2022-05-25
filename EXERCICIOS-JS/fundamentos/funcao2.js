61494b36c684a6efa01a89e65927fd2afa420bd5
andrenachtigall <an.nachtigall@inf.ufpel.edu.br>
2022-05-25 02:26:17 -0300

    javascript

---
+++ EXERCICIOS-JS/fundamentos/funcao2.js
@@ -0,0 +1,18 @@
+// Armazenando uma função em uma variavel
+const imprimirSoma = function (a,b){
+    console.log(a + b);
+}
+imprimirSoma(2,3)
+
+// Armazenando uma função arrow em uma variavel
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(2, 3)); 
+
+// returno implicito
+
+const substracao = (a, b) => a - b;
+
+console.log(substracao(2, 3))
